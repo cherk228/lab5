73c0342fd9debd51c639208e5abbb3aaea7a551f
nikitacerkasov <cerkasov450@gmail.com>
2022-04-25 18:33:07 +0300

    exeptionUp

--- src/WrongAmountOfElementsInCommandExeption.java
+++ src/WrongAmountOfElementsInCommandExeption.java
@@ -3,6 +3,10 @@ public class WrongAmountOfElementsInCommandExeption extends Exception{
         public WrongAmountOfElementsInCommandExeption(String message){
             super(message);
         }
+        public void addExepcion()
+        {
+            System.out.println("hello");
+        }
 }
 
 
