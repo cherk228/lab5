1bf17d9c88817e7aff3b15a8fc71e62fe5903328
nikitacerkasov <cerkasov450@gmail.com>
2022-04-25 18:37:56 +0300

    hueta

--- src/WrongAmountOfElementsInCommandExeption.java
+++ src/WrongAmountOfElementsInCommandExeption.java
@@ -3,10 +3,7 @@ public class WrongAmountOfElementsInCommandExeption extends Exception{
         public WrongAmountOfElementsInCommandExeption(String message){
             super(message);
         }
-        public void addExepcion()
-        {
-            System.out.println("hello");
-        }
+
 }
 
 
