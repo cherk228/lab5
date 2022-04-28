0b9de73a0f1309a885be43757cb3f81d862309e8
Nikita <cerkasov450@gmail.com>
2022-04-28 20:26:11 +0300

    nachalParsing

--- src/main/lab5.java
+++ src/main/lab5.java
@@ -1,7 +1,23 @@
 package main;
 
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 public class lab5 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException {
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        SAXParser saxParser = spf.newSAXParser();
+        XMLReader xmlReader = saxParser.getXMLReader();
+
+
+    }
+
+    private static class MyHandler extends DefaultHandler{
 
     }
 }
